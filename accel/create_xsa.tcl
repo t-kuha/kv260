c459caeb367944c5b6b40c4a7212c9d262a3c9a6
t-kuha <imagingtechnerd@gmail.com>
2024-06-29 14:11:14 +0900

    Add HW source files

---
+++ accel/create_xsa.tcl
@@ -0,0 +1,59 @@
+# Setting
+set PRJ_DIR     _vivado
+set PRJ_NAME    kv260
+set BD_NAME     ${PRJ_NAME}
+set SRC_DIR     src
+
+# Add board repo path
+set_param board.repoPaths $::env(RDI_DATADIR)/xhub/boards/XilinxBoardStore/boards/Xilinx
+
+# Remove existing directory
+file delete -force ${PRJ_DIR}
+
+# Create project
+create_project ${PRJ_NAME} ${PRJ_DIR} -part xck26-sfvc784-2LV-c
+set_property board_part xilinx.com:kv260_som:part0:1.3 [current_project]
+set_property board_connections {som240_1_connector xilinx.com:kv260_carrier:som240_1_connector:1.3} [current_project]
+set_property platform.extensible "true" [current_project]
+
+# Add constraint
+add_files -fileset constrs_1 -norecurse ${SRC_DIR}/default.xdc
+
+# Create block design
+source ${SRC_DIR}/bd.tcl
+
+# Set top-level source
+make_wrapper -files [get_files [current_bd_design].bd] -top
+add_files -norecurse ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/hdl/${BD_NAME}_wrapper.v
+set_property top ${BD_NAME}_wrapper [current_fileset]
+update_compile_order -fileset sources_1
+
+# Generate block design
+regenerate_bd_layout
+validate_bd_design
+save_bd_design
+generate_target all [get_files [current_bd_design].bd]
+
+# generate bitstream
+launch_runs synth_1
+wait_on_run synth_1
+launch_runs impl_1
+wait_on_run impl_1
+launch_runs impl_1 -to_step write_bitstream
+wait_on_run impl_1
+
+# Export .xsa file
+set_property platform.design_intent.embedded {true} [current_project]
+set_property platform.design_intent.server_managed {false} [current_project]
+set_property platform.design_intent.external_host {false} [current_project]
+set_property platform.design_intent.datacenter {false} [current_project]
+set_property platform.default_output_type {sd_card} [current_project]
+set_property platform.uses_pr {false} [current_project]
+
+write_hw_platform -hw -force -file hw.xsa
+write_hw_platform -hw_emu -force -file hw_emu.xsa
+validate_hw_platform hw.xsa
+validate_hw_platform hw_emu.xsa
+
+# Finish - close project
+close_project
