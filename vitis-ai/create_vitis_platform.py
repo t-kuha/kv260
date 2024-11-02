bad42b542ee2c7237afa2868f38f6ed8274ac4d2
t-kuha <imagingtechnerd@gmail.com>
2024-11-02 21:57:37 +0900

    Update Python script for creating platform

--- vitis-ai/create_vitis_platform.py
+++ vitis-ai/create_vitis_platform.py
@@ -1,6 +1,5 @@
 '''Create acceleration platform using BSP from AMD/Xilinx.
 '''
-import argparse
 import os
 import shutil
 import vitis
@@ -14,15 +13,8 @@ sd_dir = os.path.join(top_dir, '_sd_dir')
 hw_xsa_path = os.path.join(top_dir, 'hw.xsa')
 hw_emu_xsa_path = os.path.join(top_dir, 'hw_emu.xsa')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('bsp_path', type=str, help='path to KV260 Petalinux BSP')
-args = parser.parse_args()
-
-if not os.path.exists(args.bsp_path):
-    raise FileNotFoundError(f'{args.bsp_path = :}')
-
 # path to source files
-images_dir_path = os.path.join(args.bsp_path, 'pre-built', 'linux', 'images')
+images_dir_path = os.path.join(top_dir, 'petalinux', 'images', 'linux')
 
 # prepare required files
 for d in [pfm_dir, boot_dir, sd_dir]:
@@ -43,7 +35,7 @@ shutil.copy(
     os.path.join(boot_dir, 'u-boot.elf')
 )
 
-for fn in ['boot.scr', 'Image', 'ramdisk.cpio.gz.u-boot', 'system-zynqmp-sck-kv-g-revB.dtb', 'system.dtb']:
+for fn in ['boot.scr', 'Image', 'ramdisk.cpio.gz.u-boot']:  #, 'system-zynqmp-sck-kv-g-revB.dtb', 'system.dtb']:
     shutil.copy(os.path.join(images_dir_path, fn), sd_dir)
 
 # generate platform
@@ -59,15 +51,11 @@ platform = client.create_platform_component(
 domain = platform.get_domain(name='linux_psu_cortexa53')
 status = domain.update_name(new_name='xrt')
 assert status
-status = domain.generate_bif()
-assert status
 status = domain.add_boot_dir(images_dir_path)
 status = domain.add_boot_dir(boot_dir)
 assert status
 status = domain.set_sd_dir(sd_dir)
 assert status
-# status = domain.set_dtb(path=dtb_path)
-# assert status
 platform = client.get_platform_component(name=PFM_NAME)
 
 platform.remove_boot_bsp()
