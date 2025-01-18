3ca51300208d2497ff71760f0d3a78b2718eb2c2
t-kuha <imagingtechnerd@gmail.com>
2025-01-18 12:25:54 +0000

    Add sources for creating platform & vadd example

---
+++ accel/create_vitis_platform.py
@@ -0,0 +1,68 @@
+'''Create acceleration platform using BSP from AMD/Xilinx.
+'''
+# import argparse
+import os
+import shutil
+import vitis
+
+
+PFM_NAME = 'kv260'
+top_dir = os.path.dirname(os.path.abspath(__file__))
+pfm_dir = os.path.join(top_dir, '_pfm')
+boot_dir = os.path.join(top_dir, '_boot')
+sd_dir = os.path.join(top_dir, '_sd_dir')
+hw_xsa_path = os.path.join(top_dir, 'hw.xsa')
+# hw_emu_xsa_path = os.path.join(top_dir, 'hw_emu.xsa')
+
+
+# path to source files
+images_dir_path = os.path.join(top_dir, 'petalinux', 'images', 'linux')
+
+# prepare required files
+for d in [pfm_dir, boot_dir, sd_dir]:
+    if os.path.exists(d):
+        shutil.rmtree(d)
+    os.mkdir(d)
+
+for fn in ['bl31.elf', 'pmufw.elf']:
+    shutil.copy(os.path.join(images_dir_path, fn), boot_dir)
+
+# need renaming
+shutil.copy(
+    os.path.join(images_dir_path, 'zynqmp_fsbl.elf'),
+    os.path.join(boot_dir, 'fsbl.elf')
+)
+shutil.copy(
+    os.path.join(images_dir_path, 'u-boot-dtb.elf'),
+    os.path.join(boot_dir, 'u-boot.elf')
+)
+
+# for fn in ['boot.scr', 'Image', 'ramdisk.cpio.gz.u-boot', 'system-zynqmp-sck-kv-g-revB.dtb', 'system.dtb']:
+for fn in ['boot.scr', 'Image', 'ramdisk.cpio.gz.u-boot', 'system.dtb']:
+    shutil.copy(os.path.join(images_dir_path, fn), sd_dir)
+
+# generate platform
+client = vitis.create_client()
+client.set_workspace(path=pfm_dir)
+
+platform = client.create_platform_component(
+    name=PFM_NAME, hw_design=hw_xsa_path, desc='KV260 Vitis acceleration platform',
+    os='linux', cpu='psu_cortexa53', no_boot_bsp=True,
+    # emu_design=hw_emu_xsa_path
+)
+
+domain = platform.get_domain(name='linux_psu_cortexa53')
+assert domain.update_name(new_name='xrt')
+assert domain.generate_bif()
+assert domain.set_boot_dir(boot_dir)
+assert domain.set_sd_dir(sd_dir)
+
+platform.remove_boot_bsp()
+status = platform.build()
+
+print('----- domain.report')
+domain.report()
+print('----- platform.report')
+platform.report()
+
+vitis.dispose()
